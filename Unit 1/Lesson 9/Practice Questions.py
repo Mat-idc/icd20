04932a48667cedb91ff0ad4a6c250e472770e2ed
Mathew Reusing <mreusing@bayviewglen.ca>
2023-10-14 18:32:34 -0400

    Practice question for test Lesson 9

---
+++ Unit 1/Lesson 9/Practice Questions.py
@@ -0,0 +1,76 @@
+#task 1
+
+'''print("Hello, Mathew!")
+
+#task 5
+
+num1 = 25
+num2 = 7
+
+a = num1/num2
+
+print(f"{a:.3f}")
+
+#task 4 a
+
+import math
+
+print(f"{math.pi:.3f}")
+
+#task 4 b
+
+h = "hi"
+
+print(f"{h:>20}")
+
+#task 7
+
+user_input = float(input("Please enter a float number"))
+
+a = int(user_input)
+
+print(f"{a}")
+
+#task 9
+
+a = float(input("Enter a float number:"))
+b = float(input("Enter a float number:"))     
+
+c = a+b/2
+print(f"{c:.2f}")
+
+#task 6
+import math 
+
+a = int(input("Please enter integer:"))
+
+b = math.sqrt(a)
+
+print(f"{b:.2f}")
+
+
+#task 9
+a = (float(input("Please enter a float number:")))
+
+b = int(a)
+
+print(f"{a} {b}")
+
+#task 3
+
+a = 14*2
+print = (f"My name is Mathew I am {a} years old.")'''
+
+# creating a table 1
+
+item = "Product"
+price = 25.99
+quantity = 3
+total = price*quantity
+
+print(f"{'Item':>1} {'Price':>9} {'Quantity':>10} {'Total':>8} ")
+print(f"{item:>1} {price:>6} {quantity:>3} {total:>15}")
+
+a = (float(input("Please enter a float number")))
+
+print(f"{a:.10%}")
