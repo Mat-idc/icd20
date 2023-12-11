ecd89b3a707ce1184c0e285e92af49c4b55123a4
Mathew Reusing <mreusing@bayviewglen.ca>
2023-12-11 12:53:34 -0500

    Superhero Adventure Assignment Submission Final

--- Unit 3/Lesson 7/Super Hero Adventure.py
+++ Unit 3/Lesson 7/Super Hero Adventure.py
@@ -1,8 +1,8 @@
-#calls important functions and states global variables
+#This imports a library and states the global variables
 import random
 score = 0
-current_health = 100 
 game_running = True
+current_health = 100 
 
 #Task 1 choose superhero returns the number of the superhero selected by the player 
 def choose_superhero():
@@ -21,7 +21,7 @@ def game_intro(player_superhero):
     else:
         print(f"Welcome Spiderman, to the city of New York! You will be tasked with defeating Thanos")
 
-#Task 3 allows user to choose whether to faceoff or get backup
+#Task 3 allows the user to choose whether to faceoff alone or get backup to fight Thanos
 def make_decision():
     while True:
             print("Thanos is destroying central park! Do you go to stop him, or seek help from the sourcerer, Dr. Strange")  
@@ -32,9 +32,10 @@ def make_decision():
                 print("invalid input")
                 print()
 
-#Task 4 Responds to whether user got backup or chose to faceoff Thanos alone also gives options for attacks if they chose to faceoff alone
-def superhero_mission(action, player_superhero):      
+#Task 4 responds to whether the user chose to get backup or to faceoff Thanos alone it also gives options for attacks if they chose to faceoff alone
+def superhero_mission(action, player_superhero):     
     if action=="1":
+        current_health = health_managment()
         if player_superhero=="1" or "2" or "3": 
             print("You find Thanos armed and ready for your attack. Prepare to fight.")
             running = True
@@ -42,42 +43,46 @@ def superhero_mission(action, player_superhero):
                 attack = input("What attack would you like to do? 1 for stroke, 2 for punch, 3 for swing.")
                 if attack=="1":
                     if random.randint(0,100)<70:
-                        print("Your stroke finishes Thanos off. You return home victorious.")                        
+                        print(f"Your stroke finishes Thanos off. You return home victorious with {current_health} health left.")                        
                         return "20"
                     else:
                         print("Your attack failed, leaving you vunerable to a counterattack. You don't return home.")
-                        return "0"                    
+                        return "0 and your health is 0"                   
                     running = False
                 elif attack=="2" or attack=="3":
                     if random.randint(0,100)<60:
-                        print("your punch connects and finishes Thanos off. You return home victorious.")
+                        #current_health = health_managment()
+                        print(f"your punch connects and finishes Thanos off. You return home victorious with {current_health} health left.")
                         return "10"
-                    else:
+                    else:                        
                         print("Your attack failed, leaving you vunerable to a counterattack. You don't return home.")
-                        return "0"
+                        return "0 and your health is 0"
                     running = False                    
                 else:
                     print("invalid input")
                     
     elif action=="2":
+        current_health = health_managment()
         if player_superhero=="1":
-            print("You and Dr.Strange team up with the Gaurdians of the Galaxy. You suprise attack Thanos and he is out numbered. You all return home victorious!")
+            print(f"You and Dr.Strange team up with the Gaurdians of the Galaxy. You suprise attack Thanos and he is out numbered. You have {current_health} health left!")
             return "20"
         else:
             print("Thanos brings backup. Your attack failed and Dr. Strange is knocked out. You don't return home.")
-    return "0"
+    return "0 and your health is 0"
 
 #Task 5 determines health of superhero
-def health_managment(current_health, damage_taken):
+def health_managment():
+    damage_taken = random.randint(0,100)
     return current_health-damage_taken
 
-#main code (calls functions)
+#This is the main code
+print(f"You start with {current_health} health")
 player_name = choose_superhero()
 game_intro(player_name)
 decision = make_decision()
 score = superhero_mission(decision, player_name)
 print(f"Your Final Score Was {score}!")
 
-#ERROR how do I get the code to calculate health and is this necessary since we are only simulating one round        
+
 
     
